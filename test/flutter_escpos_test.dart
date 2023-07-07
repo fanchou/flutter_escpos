abc8b891f46cf70d8c752575a8141a7e54120ef7
zhoufan <zhoufan@freshfans.cn>
2023-07-07 09:46:54 +0800

    增加zpl指令

--- test/flutter_escpos_test.dart
+++ test/flutter_escpos_test.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_escpos/flutter_escpos.dart';
-
-void main() {
-  test('adds one to input values', () {
-
-  });
-}
+// import 'package:flutter_test/flutter_test.dart';
+//
+// import 'package:flutter_escpos/flutter_escpos.dart';
+//
+// void main() {
+//   test('adds one to input values', () {
+//
+//   });
+// }
