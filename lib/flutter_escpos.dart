440fbc505e6fc42db36b2b83daf54ab53c16e2d0
zhoufan <zhoufan@freshfans.cn>
2023-08-29 11:54:13 +0800

    add usb to serial

--- lib/flutter_escpos.dart
+++ lib/flutter_escpos.dart
@@ -10,6 +10,7 @@ export './src/adapter/usb_printer_manager.dart';
 export './src/adapter/networkAdapter.dart';
 export './src/adapter/network_printer_manager.dart';
 export './src/adapter/bluetooth_printer_manager.dart';
+export './src/adapter/usbToSerial_printer_manager.dart';
 export './src/adapter/serialportAdapter.dart';
 export './src/model/usb_printer.dart';
 export './src/model/network_printer.dart';
